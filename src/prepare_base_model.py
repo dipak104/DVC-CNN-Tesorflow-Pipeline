e3d04dcfffe7afed0833ab929e43968a78e68be6
Dipak Tripathi <dipaktripathi10@gmail.com>
2022-04-07 09:36:45 +0530

    Stage 2 updated with ffull model creation

--- src/prepare_base_model.py
+++ src/prepare_base_model.py
@@ -2,8 +2,8 @@ import argparse
 import os
 from tqdm import tqdm
 import logging
-from src.utils.common import read_yaml, create_directories
-from src.utils.model import get_VGG16_model, prepare_base_model
+from src.utils.common import read_yaml, create_directory
+from src.utils.model import get_VGG16_model, prepare_full_model
 
 
 STAGE = "PREPARE BASE MODEL" ## <<< change stage name 
@@ -18,7 +18,7 @@ logging.basicConfig(
 
 def prepare_base_model(config_path: str, params_path: str) -> None:
     config = read_yaml(config_path)
-    params = read_yaml(config_path)
+    params = read_yaml(params_path)
 
     artifacts = config["artifacts"]
     artifacts_dir = artifacts["ARTIFACTS_DIR"]
@@ -27,23 +27,25 @@ def prepare_base_model(config_path: str, params_path: str) -> None:
     base_model_name = artifacts["BASE_MODEL_NAME"]
 
     base_model_dir_path = os.path.join(artifacts_dir, base_model_dir)
-    create_directories([base_model_dir_path])
+    create_directory([base_model_dir_path])
 
-    base_model_path = o.path.join(base_model_dir_path, base_model_name)
+    base_model_path = os.path.join(base_model_dir_path, base_model_name)
 
-    base_model = get_VGG16_model()
+    base_model = get_VGG16_model(input_shape=params["IMAGE_SIZE"], model_path=base_model_path)
 
-    full_model = prepare_full_model(base_model)
+    full_model = prepare_full_model(base_model, learning_rate=params["LEARNING_RATE"], CLASSES=2, freeze_all=True, freeze_till=None)
 
-    updated_base_model_path  = os.path.join(base_model_dir_path, artifaccts["UPDATED_BASE_MODEL_NAME"])
+    updated_full_model_path  = os.path.join(base_model_dir_path, artifacts["UPDATED_BASE_MODEL_NAME"])
 
-    full_model.save(updated_base_model_path)
+    full_model.save(updated_full_model_path)
+
+
+    logging.info(f"Full Untrained model is saved at {updated_full_model_path}")
 
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument("--config", "-c", default="configs/config.yaml")
     args.add_argument("--config", "-c", default="configs/config.yaml")
     args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
--- src/utils/model.py
+++ src/utils/model.py
@@ -1,8 +1,51 @@
+import tensorflow as tf
+import logging
 
 
+def get_VGG16_model(input_shape: list, model_path: str) -> tf.keras.models.Model:
+    # """
+    # Saving and returning the base model extracted from VGG16 model
+    # Args:
+    #     input_shape (list): shape of the input images
+    #     model_path (str): path to save the base model
+    # Returns:
+    #     tf.python.keras.engine.functional.Functional: Return Type
+    # """
+    model = tf.keras.applications.vgg16.VGG16(input_shape=input_shape, weights='imagenet', include_top=False)
+    model.save(model_path)
+    logging.info(f"VGG16 model saved at: {model_path}")
+    return model
 
-def get_VGG16_model():
-    pass
+def prepare_full_model(base_model, learning_rate, CLASSES=2, freeze_all=True, freeze_till=None) -> tf.keras.models.Model:
+    # """Prepares the complete transfer learning model
+    # Args:
+    #     base_model (tf.keras.models.Model): base VGG16 model
+    #     learning_rate (float): learning rate
+    #     CLASSES (int, optional):  Defaults to 2.
+    #     freeze_all (bool, optional): _description_. Defaults to True.
+    #     freeze_till (_type_, optional): _description_. Defaults to None.
 
-def prpepare_base_model(model):
-    pass
+    # Returns:
+    #     tf.keras.models.Model: _description_
+    # """
+    if freeze_all:
+        for layer in base_model.layers:
+            layer.trainable = False
+    elif (freeze_till is not None) and (freeze_till > 0):
+        for layer in base_model.layers[:-freeze_till]:
+            layer.trainable = False
+
+    ## Add new layers in the existing base model
+    flatten_in = tf.keras.layers.Flatten()(base_model.output)
+
+    prediction = tf.keras.layers.Dense(units = CLASSES, activation='softmax')(flatten_in)
+
+    full_model = tf.keras.models.Model(inputs = base_model.input, outputs = prediction)
+
+    full_model.compile(optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate), loss=tf.keras.losses.CategoricalCrossentropy(), metrics=['accuracy'])
+
+    logging.info(f"Custom model is compiled and ready to train")
+
+    full_model.summary()
+
+    return full_model
