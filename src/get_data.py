b698d53f95ce5bd96e6c496f994495f56e7af4ad
Dipak Tripathi <dipaktripathi10@gmail.com>
2022-04-07 06:51:37 +0530

    stage prepare base model file ready

--- src/get_data.py
+++ src/get_data.py
@@ -7,16 +7,14 @@ from src.utils.common import read_yaml, create_directory, copy_files
 import random
 
 
-STAGE = "GET_DATA" ## <<< change stage name 
+STAGE = "GET_DATA"  ## <<< change stage name
 
 logging.basicConfig(
-    filename=os.path.join("logs", 'running_logs.log'), 
-    level=logging.INFO, 
+    filename=os.path.join("logs", "running_logs.log"),
+    level=logging.INFO,
     format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
-    filemode="a"
-    )
-
-
+    filemode="a",
+)
 
 
 def get_data(config_path):
@@ -24,12 +22,17 @@ def get_data(config_path):
     source_download_dir = config["source_download_dirs"]
     local_data_dir = config["local_data_dirs"]
 
-    for source, local in tqdm(zip(source_download_dir, local_data_dir), total=2, desc="list of folders", colour="red"):
+    for source, local in tqdm(
+        zip(source_download_dir, local_data_dir),
+        total=2,
+        desc="list of folders",
+        colour="red",
+    ):
         create_directory([local])
         copy_files(source, local)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
     parsed_args = args.parse_args()
